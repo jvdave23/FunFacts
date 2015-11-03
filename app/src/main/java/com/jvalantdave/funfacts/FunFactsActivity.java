aee27337b857bc74d2e0e0b784b8bf1176b59e5a
pivotal-jvalant-dave <jdave@pivotal.io>
2015-11-03 13:51:18 -0500

    started to implement array

--- app/src/main/java/com/jvalantdave/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/jvalantdave/funfacts/FunFactsActivity.java
@@ -26,10 +26,29 @@ public class FunFactsActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 // the button was clicked, so update the fact label with a new fact
-                String fact = "";
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "Banging your head against a wall burns 150 calories per hour.",
+                        "When hippos are upset, their sweat turns red.",
+                        "If you consistently fart for 6 years and 9 months, enough gas is produced to smell like Winthya",
+                        "You cannot snore and dream at the same time.",
+                        "Samiya is also a synonym for llama. ",
+                        "Jason is a pundai.",
+                        "Vithushan has never made fantasy playoffs.",
+                        "Sujee is the Los Angeles Clippers of our fantasy league.",
+                        "Prez is the Meek Mill of this generation.",
+                        "Glenn is just plain shit at fantasy.",
+                        "JV will win the 2015-2016 fantasy league. He's also an overall swell guy.",
+                        "Lucksson gonna take 3 D's when he faces JV in the playoffs. ",
+                        "Sneha is a dingus"
+
+                };
+
                 //randomly select a fact
                 Random randomGenerator = new Random(); // construct a new Random number generator
-                int randomNumber = randomGenerator.nextInt(3);
+                int randomNumber = randomGenerator.nextInt(16);
 
                 /* Convert the randomNumber to a text fact
                  0 = Ants stretch when they wake up in the morning
@@ -52,57 +71,60 @@ public class FunFactsActivity extends AppCompatActivity {
 
                  */
 
-                if (randomNumber == 0) {
+                /*if (randomNumber == 0) {
                     fact = "Ants stretch when they wake up in the morning";
                 }
-                if (randomNumber == 1) {
+                else if (randomNumber == 1) {
                     fact = "Ostriches can run faster than horses.";
                 }
-                if (randomNumber == 2) {
+                else if (randomNumber == 2) {
                     fact = "Olympic gold medals are actually made mostly of silver.";
                 }
-                if (randomNumber == 3) {
+                else if (randomNumber == 3) {
                     fact = "Banging your head against a wall burns 150 calories per hour.";
                 }
-                if (randomNumber == 4) {
+                else if (randomNumber == 4) {
                     fact = "When hippos are upset, their sweat turns red.";
                 }
-                if (randomNumber == 5) {
+                else if (randomNumber == 5) {
                     fact = "If you consistently fart for 6 years and 9 months, enough gas is produced to smell like Winthya";
                 }
-                if (randomNumber == 6) {
+                else if (randomNumber == 6) {
                     fact = "You cannot snore and dream at the same time.";
                 }
-                if (randomNumber == 7) {
+                else if (randomNumber == 7) {
                     fact = "Samiya is also a synonym for llama. ";
                 }
-                if (randomNumber == 8) {
+                else if (randomNumber == 8) {
                     fact = "Jason is a pundai.";
                 }
-                if (randomNumber == 9) {
+                else if (randomNumber == 9) {
                     fact = "Vithushan has never made fantasy playoffs.";
                 }
-                if (randomNumber == 10) {
+                else if (randomNumber == 10) {
                     fact = "Sujee is the Los Angeles Clippers of our fantasy league.";
                 }
-                if (randomNumber == 11) {
+                else if (randomNumber == 11) {
                     fact = "Prez is the Tupac of this generation.";
                 }
-                if (randomNumber == 12) {
+                else if (randomNumber == 12) {
                     fact = "Glenn is just plain shit at fantasy.";
                 }
-                if (randomNumber == 13) {
+                else if (randomNumber == 13) {
                     fact = "JV will win the 2015-2016 fantasy league. He's also an overall swell guy.";
                 }
-                if (randomNumber == 14) {
+                else if (randomNumber == 14) {
                     fact = "Lucksson gonna take 3 D's when he faces JV in the playoffs. ";
                 }
-                if (randomNumber == 15) {
+                else if (randomNumber == 15) {
                     fact = "Sneha is a dingus";
                 }
+                else {
 
+                }
+                */
                 //update the label with our dynamic fact
-                factLabel.setText(fact);
+                factLabel.setText(facts);
             }
         };
         showFactButton.setOnClickListener(listener);
