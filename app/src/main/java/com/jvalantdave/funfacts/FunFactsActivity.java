0f000bd8fae2b1f4c177769d4686b4b5a581fa30
jvdave23 <jvdave23@gmail.com>
2015-11-07 16:39:07 -0500

    both sides of code match now

--- app/src/main/java/com/jvalantdave/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/jvalantdave/funfacts/FunFactsActivity.java
@@ -27,34 +27,7 @@ public class FunFactsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-<<<<<<< HEAD
-                // the button was clicked, so update the fact label with a new fact
-                String fact = "";
-                //randomly select a fact
-                Random randomGenerator = new Random(); // construct a new Random number generator
-                int randomNumber = randomGenerator.nextInt(16);
-
-                /* Convert the randomNumber to a text fact
-                 0 = Ants stretch when they wake up in the morning
-                 1 = Ostriches can run faster than horses.
-                 2 = Olympic gold medals are actually made mostly of silver.
-                 3 = Banging your head against a wall burns 150 calories per hour.
-                 4 = When hippos are upset, their sweat turns red.
-                 5 = If you consistently fart for 6 years and 9 months, enough gas is produced to create the energy of an atomic bomb.
-                 6 = You cannot snore and dream at the same time.
-                 7 = A toaster uses almost half as much energy as a full-sized over.
-                 8 = Jason is a pundai.
-                 9 = Vithushan has never made fantasy playoffs.
-                 10 = Sujee is the Los Angeles Clippers of our fantasy league.
-                 11 = Prez is the Tupac of this generation.
-                 12 = Glenn is just plain shit at fantasy.
-                 13 = JV will win the 2015-2016 fantasy league.
-                 14 = Lucksson thinks he ripped off JV with that Howard trade last year but he doesn't know it's all part of the plan.
-                 15 = Sneha is a dingus
-
-=======
                 String fact = mFactBook.getFact();
->>>>>>> origin/master
 
                 // the button was clicked, so update the fact label with a new fact
                 factLabel.setText(fact);
@@ -64,6 +37,5 @@ public class FunFactsActivity extends AppCompatActivity {
         };
         showFactButton.setOnClickListener(listener);
     }
-
-
 }
+
