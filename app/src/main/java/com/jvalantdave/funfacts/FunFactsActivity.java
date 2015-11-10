1ea8d5699ed393c264b2304a2b5a77c19e36d90d
pivotal-jvalant-dave <jdave@pivotal.io>
2015-11-10 13:04:13 -0500

    Revert "almost complete"
    This reverts commit b08dcdf77246c4c99591833a336d703f07ee7dd4.

--- app/src/main/java/com/jvalantdave/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/jvalantdave/funfacts/FunFactsActivity.java
@@ -1,6 +1,5 @@
 package com.jvalantdave.funfacts;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,14 +7,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
+import java.util.Random;
 
 
 public class FunFactsActivity extends AppCompatActivity {
 
     private FactBook mFactBook = new FactBook();
-    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +22,7 @@ public class FunFactsActivity extends AppCompatActivity {
 
         // declare our view variables
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-
-        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
-        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
-
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,12 +32,7 @@ public class FunFactsActivity extends AppCompatActivity {
                 // the button was clicked, so update the fact label with a new fact
                 factLabel.setText(fact);
 
-                int color = mColorWheel.getColor();
-                relativeLayout.setBackgroundColor(color);
-                showFactButton.setTextColor(color);
-
-
-
+                mFactBook.mFacts[0] = "Pigs are cool";
             }
         };
         showFactButton.setOnClickListener(listener);
